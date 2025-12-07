681f3e184c7228c7946f5699c134d37c26013b63
Andrea Bergia <andreabergia@gmail.com>
2025-12-07 21:51:32 +0100

    backport Mozilla/rhino@425cad9ce: Handle incorrect destructuring in for loops at parse time (#2190)

--- src/main/java/dev/latvian/mods/rhino/Parser.java
+++ src/main/java/dev/latvian/mods/rhino/Parser.java
@@ -1479,6 +1479,16 @@ public class Parser {
 				inPos = ts.tokenBeg - forPos;
 				cond = expr();  // object over which we're iterating
 			} else {  // ordinary for-loop
+				// For ordinary for loops, destructuring declarations must have initializers
+				if (init instanceof VariableDeclaration) {
+					VariableDeclaration varDecl = (VariableDeclaration) init;
+					for (VariableInitializer vi : varDecl.getVariables()) {
+						if (vi.isDestructuring() && vi.getInitializer() == null) {
+							reportError("msg.destruct.assign.no.init");
+						}
+					}
+				}
+
 				mustMatchToken(Token.SEMI, "msg.no.semi.for", true);
 				if (peekToken() == Token.SEMI) {
 					// no loop condition
